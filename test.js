1694ecc20d866d881f24c0b852e7779571bd1d48
Ubuntu <ubuntu@ip-172-31-34-231.ec2.internal>
2025-08-21 19:39:50 +0000

    this is my again

--- test.js
+++ test.js
@@ -1,42 +1,38 @@
-// Requiring module
-const assert = require('assert');
-
-// We can group similar tests inside a describe block
 describe("Simple Calculations", () => {
-before(() => {
-	console.log( "This part executes once before all tests" );
+  beforeAll(() => {
+    console.log("This part executes once before all tests");
+  });
+
+  afterAll(() => {
+    console.log("This part executes once after all tests");
+  });
+
+  describe("Test1", () => {
+    beforeEach(() => {
+      console.log("executes before every test");
+    });
+
+    test("Is returning 5 when adding 2 + 3", () => {
+      expect(2 + 3).toBe(5);
+    });
+
+    test("Is returning 6 when multiplying 2 * 3", () => {
+      expect(2 * 3).toBe(6);
+    });
+  });
+
+  describe("Test2", () => {
+    beforeEach(() => {
+      console.log("executes before every test");
+    });
+
+    test("Is returning 5 when adding 2 + 3", () => {
+      expect(2 + 3).toBe(5);
+    });
+
+    test("Is returning 8 when multiplying 2 * 4", () => {
+      expect(2 * 4).toBe(8);
+    });
+  });
 });
 
-after(() => {
-	console.log( "This part executes once after all tests" );
-});
-	
-// We can add nested blocks for different tests
-describe( "Test1", () => {
-	beforeEach(() => {
-	console.log( "executes before every test" );
-	});
-	
-	it("Is returning 5 when adding 2 + 3", () => {
-	assert.equal(2 + 3, 5);
-	});
-
-	it("Is returning 6 when multiplying 2 * 3", () => {
-	assert.equal(2*3, 6);
-	});
-});
-
-describe("Test2", () => {
-	beforeEach(() => {
-	console.log( "executes before every test" );
-	});
-	
-	it("Is returning 4 when adding 2 + 3", () => {
-	assert.equal(2 + 3, 5);
-	});
-
-	it("Is returning 8 when multiplying 2 * 4", () => {
-	assert.equal(2*4, 8);
-	});
-});
-});
